99dba55b7fd3f56031e967a89f163f37b0c7e7e8
camfeitosa <camilafeitosacoutinho@gmail.com>
2024-12-02 21:35:12 -0300

    texto

--- src/components/Sobre.jsx
+++ src/components/Sobre.jsx
@@ -16,7 +16,7 @@ export function Sobre() {
             Defendendo Seus Direitos com Excelência e Integridade
           </h2>
           <p className="lg:w-[80%]">
-          Com uma sólida formação em Administração de Empresas e Direito pela FMU-FAM SP e inscrito na OAB-SP, o advogado Agnaldo Alves de Siqueira combina expertise jurídica e visão estratégica para oferecer soluções completas e personalizadas aos seus clientes. Atuando nas áreas de Direito Trabalhista, Cívil e Criminal, seu compromisso é garantir a defesa dos direitos e interesses de cada cliente com ética, eficiência e dedicação.
+          Com uma sólida formação em Administração de Empresas e Direito pela FMU-FAM SP e inscrito na OAB-SP, o advogado Agnaldo Alves de Siqueira combina expertise jurídica e visão estratégica para oferecer soluções completas e personalizadas aos seus clientes. Atuando nas áreas de Direito Trabalhista, Civil e Criminal, seu compromisso é garantir a defesa dos direitos e interesses de cada cliente com ética, eficiência e dedicação.
           </p>
           <img src={favicon} width="200" height="200" className="w-1/5 md:w-1/6 md:mt-4" alt="Logo AAS" loading="lazy" />
         </div>
